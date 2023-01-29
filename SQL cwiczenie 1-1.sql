c653f50e13391254f29e5abd22c85bf901a510ed
Rafal <rafalfura@gmail.com>
2023-01-29 16:20:49 +0100

    Commituje cwiczenie 1 zad 1

---
+++ SQL cwiczenie 1-1.sql
@@ -0,0 +1,6 @@
+SELECT * FROM misje;
+SELECT * FROM komandosi;
+SELECT * FROM komandosinamisji;
+SELECT * FROM umiejetnoscikomandosow;
+SELECT * FROM umiejetnoscispecjalne;
+SELECT * FROM komandosi WHERE stopien LIKE '%kapral'
