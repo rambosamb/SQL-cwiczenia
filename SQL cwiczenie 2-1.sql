832a5e7a9791d95bd57627d2c5800abfc182c6f8
Rafal <rafalfura@gmail.com>
2023-01-29 15:38:17 +0100

    commituje drugie cwiczenie, 1 zadanie

---
+++ SQL cwiczenie 2-1.sql
@@ -0,0 +1,10 @@
+SELECT * FROM misje;
+SELECT * FROM komandosi;
+SELECT * FROM komandosinamisji;
+SELECT * FROM umiejetnoscikomandosow;
+SELECT * FROM umiejetnoscispecjalne;
+SELECT * FROM umiejetnoscispecjalne Inner JOIN umiejetnoscikomandosow ON umiejetnoscispecjalne.id = umiejetnoscikomandosow 
+
+
+
+ 
