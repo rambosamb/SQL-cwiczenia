20bebed423aad293d8dee7b0c5f13909e3fb581f
Rafal <rafalfura@gmail.com>
2023-01-31 01:57:35 +0100

    Commituje samodzielne cwiczenia

---
+++ Sql cwiczenia samodzielne/SQL wprowadzanie tabel.sql
@@ -0,0 +1,3 @@
+ALTER TABLE użytkownicy ADD COLUMN id_użytkownika INT AUTO_INCREMENT PRIMARY KEY;
+SELECT * FROM użytkownicy;
+INSERT INTO użytkownicy VALUES ('Rafał', 'Fura', '23', 'programista', '1')
